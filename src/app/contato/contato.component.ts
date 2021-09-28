49dc28d40549eb7e72352a1546b253b14b80ae5e
Gustavo Moreira <gustavo.ms@protonmail.com>
2021-09-28 03:19:48 -0300

    add new features

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -10,31 +10,51 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 })
 export class ContatoComponent implements OnInit {
 
-  formulario! : FormGroup;
-  contatos : Contato[] = [];
+  formulario!: FormGroup;
+  contatos: Contato[] = [];
+  colunas = ['id', 'nome', 'email', 'favorito'];
 
 
-  constructor(private contatoService : ContatoService, private fb: FormBuilder) { }
+  constructor(private contatoService: ContatoService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-
-   this.formulario = this.fb.group({
-     nome: ['', Validators.required],
-     email: ['', [Validators.email, Validators.required]]
-   })
-    
+    this.montarFormulario();
+    this.listarContatos();
   }
 
-  onSubmit(){
+  montarFormulario() {
+
+    this.formulario = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]]
+    })
+  }
+
+  listarContatos() {
+    this.contatoService.list().subscribe(response => {
+      this.contatos = response;
+    })
+  }
+
+  favoritar(contato : Contato) {
+
+    this.contatoService.favorite(contato).subscribe( response => {
+      contato.favorito = !contato.favorito;
+
+    })
+  }
+
+  onSubmit() {
 
     const formValues = this.formulario.value;
-    const contato : Contato = new Contato(formValues.nome, formValues.email); 
-    
-    this.contatoService.salvar(contato).subscribe( response => {
-      this.contatos.push(response);
+    const contato: Contato = new Contato(formValues.nome, formValues.email);
+
+    this.contatoService.salvar(contato).subscribe(response => {
+      let lista : Contato[] = [...this.contatos, response];
       console.log(this.contatos);
+      this.contatos = lista;
     })
-   
+
   }
 
 
